f586b331448a4e8cd9980eb200a2a042d1e99256
mhaheer444 <mhaheer444@gmail.com>
2024-04-22 18:28:44 +0500

    Final Commit

---
+++ app/src/main/java/com/example/navigationsample/SecondScreen.kt
@@ -0,0 +1,38 @@
+package com.example.navigationsample
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SecondScreen(name: String, navigateToFirstScreen: (String)  -> Unit) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "This is the Second Screen", fontSize = 24.sp)
+        Text(text = "Welcome $name", fontSize = 24.sp)
+        Button(onClick =  {
+            navigateToFirstScreen(name)
+        })
+        {
+            Text(text = "Go back to the First Screen")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SecondScreenPreview() {
+    SecondScreen("Manzoor",{})
+}
